f5884f601e640ff2734fec26bcaed6675d9c2a82
Shanu2144 <87467505+Shanu2144@users.noreply.github.com>
2021-08-09 21:37:43 +0530

    Add files via upload

---
+++ dict.py
@@ -0,0 +1,13 @@
+dictionary = {"Phone": "Phone is a machine that simplify our life",
+              "Tablet": "Tablet is device that help to watch thing in big screen",
+              "Mother": "Mother is love",
+              "Harry": "My super sir",}
+
+#print("Enter a word")
+#print(dictionary.keys())
+#word = input()
+#dictionary.get(word)
+#print("The meaning of your word is")
+#print(dictionary[word])
+d1=dictionary
+print(d1['Phone'])
